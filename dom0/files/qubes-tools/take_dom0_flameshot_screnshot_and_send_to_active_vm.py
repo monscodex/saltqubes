c581b667181331c0624abfd1a7f105f531e83ea7
monscodex <r2bs@protonmail.com>
2025-01-06 10:06:41 +0100

    feat(flameshot_screenshot): Open screenshot in thunar after taking it

--- dom0/files/qubes-tools/take_dom0_flameshot_screnshot_and_send_to_active_vm.py
+++ dom0/files/qubes-tools/take_dom0_flameshot_screnshot_and_send_to_active_vm.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
 
+
 from qubes_utils import get_name_of_front_vm
-import os
 from datetime import datetime
 
+from pathlib import Path
+import subprocess
 
 def main():
     vm_name = get_name_of_front_vm()
+
 
     if vm_name == "dom0":
         vm_name = "vault"
 
     date = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
 
-    output_filename = f"/tmp/{date}.png"
-    command = f"flameshot gui --raw > {output_filename} && qvm-move-to-vm {vm_name} {output_filename}"
+    output_filename = f"{date}.png"
+    screenshot_command = ["flameshot", "gui", "--raw"]
 
-    os.system(command)
+    with open(f"/tmp/{output_filename}", "w") as output_file:
+        subprocess.run(screenshot_command, stdout=output_file)
+
+    # Avoid exiting flameshot and moving an aborted screenshot
+    file_size = Path(f"/tmp/{output_filename}").stat().st_size
+    if file_size:
+        move_screenshot_to_target_vm_command = ["qvm-move-to-vm", vm_name, f"/tmp/{output_filename}"]
+        open_thunar_and_highlight_file_command = ["qvm-run", vm_name, f"thunar /home/user/QubesIncoming/dom0/{output_filename}"]
+
+        subprocess.run(move_screenshot_to_target_vm_command)
+        subprocess.run(open_thunar_and_highlight_file_command)
+
 
 if __name__ == "__main__":
     main()
